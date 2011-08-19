7499a343146e8b4a5a7b009124ac548d2d581a35
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-19 16:05:31 +0200

    more work

--- notifyAppBar.js
+++ notifyAppBar.js
@@ -5,10 +5,20 @@
 	var notifyAppBar	= function(opts){
 		opts		= opts		|| {};
 		this._barId	= opts.id	|| "";
+		var cookieName	= 'notifyAppBar'+this._barId;
 
-		console.log("built runningChrome", this._isRunningChrome())
+		if( this._isChrome() === false )		return;
+
+		if( readCookie(cookieName) === 'notified' )	return;
+		
+
+		console.log("built runningChrome", this._isChrome())
+		console.log("built cookie", readCookie(cookieName));
 		this._display();
 		
+		//createCookie(this._cookieName, "notified");
+		console.log("cookie", readCookie(this._cookieName))
+		//eraseCookie(this._cookieName)
 		//setTimeout(function(){
 		//	this._hide();
 		//}.bind(this), 2*1000)
@@ -19,7 +29,7 @@
 	notifyAppBar.prototype._display	= function(){
 		this._container	= document.createElement("div");
 		this._container.innerHTML = "Super pacmaze application in chrome appstore ?";
-		this._container.id	= "prout"; 
+		this._container.id	= "prout2"; 
 		//this._container.style.zIndex	= 9999;	// to be in front of the rest
 		//this._container.style.position	= "absolute";
 		//this._container.style.= "absolute";
@@ -34,19 +44,44 @@
 	/**
 	 * @returns {Boolean} true if the client is chrome, false otherwise
 	*/
-	notifyAppBar.prototype._isRunningChrome	= function(){
+	notifyAppBar.prototype._isChrome	= function(){
 		return navigator.userAgent.match(/chrome/i) ? true : false;
 	}
 
-	notifyAppBar.prototype._cookieWrite	= function(value){
-		document.cookie = 'notifyAppBar'+this._barId+'='+value+'; path=/; expires='+new Date(Date.now()+365*3600*24).toGMTString();
-
+	/**
+	 * Create cookie
+	 * - from http://www.quirksmode.org/js/cookies.html
+	*/
+	function createCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
 	}
-	notifyAppBar.prototype._cookieRead	= function(){
-		document.cookie = 'notifyAppBar'+this._barId+'=testcookie; path=/; expires=Thu, 2 Aug 2001 20:47:11 UTC;'
+	
+	/**
+	 * Read cookie
+	 * - from http://www.quirksmode.org/js/cookies.html
+	*/
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
 	}
-	notifyAppBar.prototype._cookieDelete	= function(){
-		this._markWrite('');
+	/**
+	 * Erase cookie
+	 * - from http://www.quirksmode.org/js/cookies.html
+	*/	
+	function eraseCookie(name) {
+		createCookie(name,"",-1);
 	}
 
 	/**
