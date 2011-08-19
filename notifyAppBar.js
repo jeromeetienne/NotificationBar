7c362a425f5e9c84e92e3733b957b17d12e8c4e4
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-19 14:59:21 +0200

    more work

--- notifyAppBar.js
+++ notifyAppBar.js
@@ -6,7 +6,7 @@
 		opts		= opts		|| {};
 		this._barId	= opts.id	|| "";
 
-		console.log("built")
+		console.log("built runningChrome", this._isRunningChrome())
 		this._display();
 		
 		//setTimeout(function(){
@@ -19,7 +19,7 @@
 	notifyAppBar.prototype._display	= function(){
 		this._container	= document.createElement("div");
 		this._container.innerHTML = "Super pacmaze application in chrome appstore ?";
-		this._container.id	= "prout";
+		this._container.id	= "prout"; 
 		//this._container.style.zIndex	= 9999;	// to be in front of the rest
 		//this._container.style.position	= "absolute";
 		//this._container.style.= "absolute";
@@ -35,17 +35,17 @@
 	 * @returns {Boolean} true if the client is chrome, false otherwise
 	*/
 	notifyAppBar.prototype._isRunningChrome	= function(){
-		return navigator.userAgent.match(/chrome/i);
+		return navigator.userAgent.match(/chrome/i) ? true : false;
 	}
 
-	notifyAppBar.prototype._markWrite	= function(value){
+	notifyAppBar.prototype._cookieWrite	= function(value){
 		document.cookie = 'notifyAppBar'+this._barId+'='+value+'; path=/; expires='+new Date(Date.now()+365*3600*24).toGMTString();
 
 	}
-	notifyAppBar.prototype._markRead	= function(){
+	notifyAppBar.prototype._cookieRead	= function(){
 		document.cookie = 'notifyAppBar'+this._barId+'=testcookie; path=/; expires=Thu, 2 Aug 2001 20:47:11 UTC;'
 	}
-	notifyAppBar.prototype._markDelete	= function(){
+	notifyAppBar.prototype._cookieDelete	= function(){
 		this._markWrite('');
 	}
 
